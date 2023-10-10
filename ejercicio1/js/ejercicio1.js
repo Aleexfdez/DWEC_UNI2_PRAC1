dfe5e2b020040e0ecedaae2ff300178fdba57f78
Aleexfdez <alexfernandez567@gmail.com>
2023-10-10 09:36:33 +0200

    ejercicio1 funciona pero no funciones

---
+++ ejercicio1/js/ejercicio1.js
@@ -0,0 +1,16 @@
+
+
+var cadena = prompt("Introduce palabra o frase");
+separarPalabras(cadena);
+
+
+function separarPalabras(cadena) {
+
+    var separarCadena = cadena.split(""); // Separa en subcadenas
+
+    var invertirCadena = separarCadena.reverse(); // Da la vuelta a la subcadema
+
+    var unirCadena = invertirCadena.join(""); // Une nuevamente en cadena las subcadenas
+
+    return console.log(unirCadena);
+}
